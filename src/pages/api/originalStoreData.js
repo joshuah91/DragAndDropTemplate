1374f2a6a2907561a9adb57103846f04a635b3ac
Joshua Balogun <joshplexus8@gmail.com>
2023-09-04 23:53:28 +0100

    test remove reset modal in production

--- src/pages/api/originalStoreData.js
+++ src/pages/api/originalStoreData.js
@@ -8,20 +8,10 @@ const originalDataFilePath = path.join(
 
 export default async function handler(req, res) {
   if (req.method === "GET") {
-    try {
-      //Read the original Data from the original JSON file
-      const originalJsonData = await fsPromises.readFile(originalDataFilePath);
-      const originalObjectData = JSON.parse(originalJsonData);
+    //Read the original Data from the original JSON file
+    const originalJsonData = await fsPromises.readFile(originalDataFilePath);
+    const originalObjectData = JSON.parse(originalJsonData);
 
-      res.status(200).json(originalObjectData);
-    } catch (error) {
-      console.error(error);
-
-      res.status(500).json({
-        response: false,
-        status: 500,
-        message: "Error fetching original data",
-      });
-    }
+    res.status(200).json(originalObjectData);
   }
 }
