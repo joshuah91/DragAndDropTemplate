a72926a0cf942929518da9426d3157ed13035e06
Joshua Balogun <joshplexus8@gmail.com>
2023-09-05 00:33:12 +0100

    testing for json server issue

--- src/pages/api/originalStoreData.js
+++ src/pages/api/originalStoreData.js
@@ -1,17 +1,17 @@
-// import fsPromises from "fs/promises";
-// import path from "path";
+import fsPromises from "fs/promises";
+import path from "path";
 
-// const originalDataFilePath = path.join(
-//   process.cwd(),
-//   "json/originalUsers.json"
-// );
+const originalDataFilePath = path.join(
+  process.cwd(),
+  "json/originalUsers.json"
+);
 
-// export default async function handler(req, res) {
-//   if (req.method === "GET") {
-//     //Read the original Data from the original JSON file
-//     const originalJsonData = await fsPromises.readFile(originalDataFilePath);
-//     const originalObjectData = JSON.parse(originalJsonData);
+export default async function handler(req, res) {
+  if (req.method === "GET") {
+    //Read the original Data from the original JSON file
+    const originalJsonData = await fsPromises.readFile(originalDataFilePath);
+    const originalObjectData = JSON.parse(originalJsonData);
 
-//     res.status(200).json(originalObjectData);
-//   }
-// }
+    res.status(200).json(originalObjectData);
+  }
+}
